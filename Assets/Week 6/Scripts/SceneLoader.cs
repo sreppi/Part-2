eb6a7d21c4d6a856e6b6b3a34091f4b5f6fc3e40
XPC\calvi <calvinip@live.ca>
2024-02-14 14:43:55 -0500

    Task 2 - Added Scene B, allowed players to use the space bar to change scenes as long as the scene loader is in the scene.

--- Assets/Week 6/Scripts/SceneLoader.cs
+++ Assets/Week 6/Scripts/SceneLoader.cs
@@ -4,14 +4,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class SceneLoader : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            LoadNextScene();
+        }
+    }
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings; // Watch out for sceneCount, it's something different || % is a modular function 
         SceneManager.LoadScene(nextSceneIndex);
-
     }
+
 }
