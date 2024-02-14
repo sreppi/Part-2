fd5a98125c257842750a6b9041dd2bd5987b4696
XPC\calvi <calvinip@live.ca>
2024-02-14 15:43:41 -0500

    Added the Lerp tutorial done in class.

---
+++ Assets/Week 6/Scripts/LerpTest.cs
@@ -0,0 +1,30 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTest : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+    public Transform startPosition;
+    public Transform endPosition;
+    public float lerpTimer;
+    public float interpolation;
+    public Color colA;
+    public Color colB;
+    public SpriteRenderer sr;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        interpolation = animationCurve.Evaluate(lerpTimer);
+        transform.position = Vector3.Lerp(startPosition.position, endPosition.position, interpolation);
+        lerpTimer += Time.deltaTime;
+        sr.color = Color.Lerp(colA, colB, interpolation);
+    }
+}
